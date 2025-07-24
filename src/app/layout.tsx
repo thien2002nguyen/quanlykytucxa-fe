ac0bb732a3a2fb85a55ae89c2a102c1548014dcd
thien2002nguyen <133083833+thien2002nguyen@users.noreply.github.com>
2025-07-24 13:50:58 +0700

    [update] seo homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,44 @@ import { ReduxProvider } from "@/store/provider";
 import { Bounce, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./globals.scss";
+import { appConfig } from "@/config/appConfig";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: appConfig.site_name,
+    template: `%s | ${appConfig.site_name}`,
+  },
+  description: appConfig.description,
+  keywords: appConfig.keywords,
+  robots: {
+    follow: true,
+    index: true,
+  },
+  manifest: "/manifest.json",
+  icons: {
+    icon: "/images/logo.ico",
+  },
+  authors: [
+    {
+      url: appConfig.url,
+      name: appConfig.title,
+    },
+  ],
+  verification: {
+    google: process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION_ID,
+  },
+  metadataBase: new URL(appConfig.url),
+  openGraph: {
+    images: `${appConfig.url}/manifest/icon-512x512.jpg`,
+    type: "article",
+    title: appConfig.title,
+    authors: appConfig.title,
+    description: appConfig.description,
+    locale: appConfig.locale,
+    siteName: appConfig.site_name,
+  },
+};
 
 export default function RootLayout({
   children,
---
+++ src/config/appConfig.ts
@@ -0,0 +1,20 @@
+const appConfig = {
+  site_name: "DAU Ký túc xá - Quản lý ký túc xá sinh viên",
+  title: "DAU Ký túc xá - Hệ thống quản lý ký túc xá trường Đại học DAU",
+  description:
+    "Hệ thống quản lý ký túc xá trường Đại học DAU hỗ trợ sinh viên đăng ký phòng, theo dõi chi phí, và cập nhật thông tin nhanh chóng, chính xác.",
+  locale: "vi",
+  url: "https://dau-kytucxa.io.vn",
+  type: "website",
+  keywords: [
+    "ký túc xá DAU",
+    "quản lý ký túc xá",
+    "ký túc xá sinh viên",
+    "hệ thống quản lý KTX",
+    "Đại học DAU",
+    "đăng ký phòng ký túc xá",
+    "quản lý sinh viên KTX",
+  ],
+};
+
+export { appConfig };
