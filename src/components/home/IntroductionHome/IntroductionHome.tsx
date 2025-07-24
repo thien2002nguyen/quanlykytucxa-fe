c0cecadf4355e6b4240e1eceed45b07757d846b5
thien2002nguyen <133083833+thien2002nguyen@users.noreply.github.com>
2025-07-24 18:11:52 +0700

    [fix] ssr

--- src/components/home/IntroductionHome/IntroductionHome.tsx
+++ src/components/home/IntroductionHome/IntroductionHome.tsx
@@ -16,7 +16,7 @@ export type Introduction = {
 };
 
 async function getIntroduction() {
-  const res = await fetch(`${process.env.INTERNAL_API_URL}/introductions`);
+  const res = await fetch(`${process.env.INTERNAL_API_URL}introductions`);
   const dataIntroduction: { data: Introduction } = await res.json();
   return dataIntroduction.data; // Trả về dữ liệu 'data' từ API
 }
